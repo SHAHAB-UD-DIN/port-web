2b8c085fe9f475aa056429fe287cdcaff20d01c1
SHAHAB-UD-DIN <150152275+SHAHAB-UD-DIN@users.noreply.github.com>
2025-02-24 20:42:17 +0500

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -6,9 +6,7 @@ export default function Contact(){
         <div className="justify-center text-center m-10">
                 <h1 className="text-base">Connect with me</h1>
                 <h2 className="text-3xl font-semibold">Get in touch</h2>
-                <p>
-                    I had love to hear from you! If you have any questions, comments or feedback.
-                    please use the form below.
+                <p>{"I'd love to hear from you! If you have any questions, comments or feedback. please use the form below."}
                 </p>
         </div>
         <SimpleForm />
